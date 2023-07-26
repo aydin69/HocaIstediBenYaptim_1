507346c6d8a6024a898020c313c448bcd6dd8cfc
aydın yavuz <aydinyavuz511@gmail.com>
2023-07-26 13:40:30 +0300

    Add project files.

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,29 @@
+﻿using Project.ENTITIES.Models;
+using Project.MAP.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base ("MyConnection")
+        {
+
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new ProfileMap());
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppUserProfile> Profiles { get; set; }
+    }
+}
